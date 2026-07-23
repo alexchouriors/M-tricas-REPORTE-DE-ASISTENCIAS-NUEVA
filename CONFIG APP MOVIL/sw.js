844a01b5d8f5caf7d4c1d412ea139a05ea2141cf
TheSantos <alexchouriors@gmail.com>
2026-07-22 23:36:20 -0400

    Update sw.js

--- CONFIG APP MOVIL/sw.js
+++ CONFIG APP MOVIL/sw.js
@@ -5,7 +5,7 @@
    Estrategia: Stale-While-Revalidate
 ================================================================ */
 
-const CACHE_NAME = 'metricas-v20';
+const CACHE_NAME = 'metricas-v21';
 
 /* Archivos pre-cacheados en la instalación.
    Las rutas son absolutas desde la raíz del sitio porque el scope es '/'. */
